e5619edb13856b215be6edbd76c9d0485fa730e5
Samuel Anozie <samuelanozie10@yahoo.com>
2022-12-23 16:53:16 +0100

    fix lint task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,11 +2,6 @@
  * Represents a class room.
  */
 export default class ClassRoom {
-    /**
-     * Creates a new @see {@link ClassRoom}.
-     * @param {Number} maxStudentsSize - The maximum number of
-     * students in the class.
-     */
     constructor(maxStudentsSize) {
         this._maxStudentsSize = maxStudentsSize;
     }
