0d81a6f1c4d6b42205da2987adaa6d825bbc8f1c
Samuel Anozie <samuelanozie10@yahoo.com>
2022-12-23 17:06:13 +0100

    fix task 0 eslint

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,7 +2,7 @@
  * Represents a class room.
  */
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
