c06b597e21946fe06816365417241998a38e9e77
Samuel Anozie <samuelanozie10@yahoo.com>
2023-01-04 11:41:58 +0100

    fix task 3

--- 0x03-ES6_data_manipulation/.eslintrc.js
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -10,11 +10,13 @@ module.exports = {
         SharedArrayBuffer: 'readonly',
     },
     parserOptions: {
-        ecmaVersion: 6,
+        ecmaVersion: 2018,
         sourceType: 'module',
     },
     plugins: ['jest'],
     rules: {
+        'max-classes-per-file': 'off',
+        'no-underscore-dangle': 'off',
         'no-console': 'off',
         'no-shadow': 'off',
         'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -8,12 +8,11 @@
  * @returns {Number}
  */
 export default function getStudentIdsSum(students) {
-    if (students instanceof Array) {
-        return students.reduce(
-            (prevStudent, curStudent) =>
-                prevStudent.id || prevStudent + curStudent.id,
-            0,
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
     );
-    }
-    return 0;
+  }
+  return 0;
 }
