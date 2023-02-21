03de9b5be60a57128e96b1234049deee623d5620
Samuel Anozie <samuelanozie10@yahoo.com>
2023-02-21 21:10:24 +0100

    task 2 nodejs basics

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Samuel Anozie <https://github.com/samcoded>
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
@@ -23,23 +23,32 @@ const countStudents = (dataPath) => {
 
   for (const line of fileLines.slice(1)) {
     const studentRecord = line.split(',');
-    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const studentPropValues = studentRecord.slice(
+      0,
+      studentRecord.length - 1,
+    );
     const field = studentRecord[studentRecord.length - 1];
     if (!Object.keys(studentGroups).includes(field)) {
       studentGroups[field] = [];
     }
-    const studentEntries = studentPropNames
-      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    const studentEntries = studentPropNames.map((propName, idx) => [
+      propName,
+      studentPropValues[idx],
+    ]);
     studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
+  const totalStudents = Object.values(studentGroups).reduce(
+    (pre, cur) => (pre || []).length + cur.length,
+  );
   console.log(`Number of students: ${totalStudents}`);
   for (const [field, group] of Object.entries(studentGroups)) {
-    const studentNames = group.map((student) => student.firstname).join(', ');
-    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    const studentNames = group
+      .map((student) => student.firstname)
+      .join(', ');
+    console.log(
+      `Number of students in ${field}: ${group.length}. List: ${studentNames}`,
+    );
   }
 };
 
